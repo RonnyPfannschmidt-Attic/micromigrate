a74d0aa75e1c9a0e28b060b24567526dc37d9211
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2014-01-31 18:35:42 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+if __name__ == '__main__': setup(
+    name='Micromigrate',
+    get_version_from_scm=True,
+    description='Minimal Migration Manager for sqlite',
+    setup_requires=[
+        'hgdistver',
+    ],
+)
