bd53f6ace7932532d8ca92093de35c3e1a75ab39
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2014-02-07 15:36:06 +0100

    add find in path

--- micromigrate/__init__.py
+++ micromigrate/__init__.py
@@ -1,2 +1,6 @@
-__all__ = ['apply_migrations', 'parse_migration']
+__all__ = [
+    'apply_migrations', 'parse_migration',
+    'find_in_path',
+]
 from .util import apply_migrations, parse_migration
+from .finder import find_in_path
