71aa31ab692cf3a2b48eba5e0a5b8a309a8b22bd
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2014-02-07 13:23:53 +0100

    move Migration type to the types module

--- micromigrate/migrate.py
+++ micromigrate/migrate.py
@@ -1,22 +1,6 @@
 from __future__ import print_function
 from hashlib import sha256
-from collections import namedtuple
-
-
-class Migration(namedtuple('MigrationBase', 'name checksum sql after')):
-    __slots__ = ()
-
-    def __repr__(self):
-        return '<Migration {name} ck {checksum}..>'.format(
-            name=self.name,
-            checksum=self.checksum[:6],
-        )
-
-    def can_apply_on(self, state):
-        return (
-            self.after is None or
-            not any(name not in state for name in self.after)
-        )
+from .types import Migration
 
 
 def parse_migration(sql):
---
+++ micromigrate/types.py
@@ -0,0 +1,17 @@
+from collections import namedtuple
+
+
+class Migration(namedtuple('MigrationBase', 'name checksum sql after')):
+    __slots__ = ()
+
+    def __repr__(self):
+        return '<Migration {name} ck {checksum}..>'.format(
+            name=self.name,
+            checksum=self.checksum[:6],
+        )
+
+    def can_apply_on(self, state):
+        return (
+            self.after is None or
+            not any(name not in state for name in self.after)
+        )
