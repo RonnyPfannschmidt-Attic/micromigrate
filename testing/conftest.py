8c42d959cc674518535ef31682a77e34ffb1d51b
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2016-09-04 17:21:24 +0200

    simplify fixtures

--- testing/conftest.py
+++ testing/conftest.py
@@ -1,41 +1,14 @@
-import os
 import pytest
 
 from micromigrate.backend_script import ScriptBackend
 from micromigrate.backend_pysqlite import PySqliteBackend
 
-
-backends = {
-    'script': ScriptBackend,
-    'binding': PySqliteBackend,
-}
-
-
-@pytest.fixture
-def dbname(request, tmpdir):
-    db = tmpdir.join('test.sqlite.db').strpath
-
-    @request.addfinalizer
-    def cleanup():
-        import subprocess
-        if os.path.exists(db):
-            subprocess.call(['sqlite3', db, '.dump'])
-    return db
+BACKENDS = [
+    ScriptBackend,
+    PySqliteBackend,
+]
 
 
-@pytest.fixture(params=sorted(backends.keys()))
-def db(request, dbname):
-    return backends[request.param].from_path(dbname)
-
-
-@pytest.hookimpl(hookwrapper=True)
-def pytest_runtest_makereport(item):
-    report = (yield).get_result()
-    dbname = getattr(item, '_dbname', None)
-    if dbname is not None:
-        from micromigrate.util import output_or_raise
-        try:
-            report.sections.append(
-                ('db', output_or_raise('sqlite3', '.dump', dbname)))
-        except:
-            pass
+@pytest.fixture(params=BACKENDS)
+def db(request, tmpdir):
+    return request.param.from_path(tmpdir.join('test.sqlite.db').strpath)
